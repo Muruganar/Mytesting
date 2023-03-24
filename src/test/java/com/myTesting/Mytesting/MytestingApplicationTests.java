f40bae30183bd8845cfadc48f654b4029f6f356c
Murugan R <1000065764@hexaware.com>
2023-03-24 10:24:03 +0530

    Initial commit

---
+++ src/test/java/com/myTesting/Mytesting/MytestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myTesting.Mytesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
